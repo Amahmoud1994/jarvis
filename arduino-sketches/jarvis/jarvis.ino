3734cd1afaaa4ba3a90a6210a0a41a4c64679951
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2017-04-18 15:33:54 +0200

    Fixing the arduino code by adding bathroom and bedroom lights and fixing the reading data style

--- arduino-sketches/jarvis/jarvis.ino
+++ arduino-sketches/jarvis/jarvis.ino
@@ -3,14 +3,16 @@
 
 char buff[100]; // we're going to use this to hold our string
 String voice;
-int lightLED = 2, //Connect LED 1 To Pin #2 
+int bedroomLED = 2,  //Connect bedroom LED 1 To Pin #2
+bathroomLED = 4,    //Connect bedroom LED 1 To Pin #4
 smokeSensor = A5,
 tempSensor = 3;
 
 
 void setup() {
     Serial.begin(9600);
-    pinMode(lightLED, OUTPUT); 
+    pinMode(bedroomLED, OUTPUT); 
+    pinMode(bathroomLED, OUTPUT); 
     pinMode(smokeSensor, INPUT); 
     pinMode(tempSensor, INPUT);
 }
@@ -18,44 +20,59 @@ void setup() {
 void loop() {
   
 
-  while (Serial.available()){  //Check if there is an available byte to read
-    delay(10); //Delay added to make thing stable 
-    char c = Serial.read(); //Conduct a serial read
-    if (c == '#') {break;} //Exit the loop when the # is detected after the word
-    voice += c; //Shorthand for voice = voice + c
+  while (Serial.available())
+  {  
+    //Check if there is an available byte to read
+    voice = Serial.readString();
+    Serial.println(voice);
+
   }  
-
-  if (voice.length() > 0) {
-
-    Serial.println(voice); 
-//-----------------------------------------------------------------------//    
+  
+  if (voice.length() > 0) 
+  {
+    
+  //-----------------------------------------------------------------------//    
 
   //----------Get the Gas data to mobile----------// 
-  if(voice == "*Send smoke ratio") {
-    int analogSensor = analogRead(smokeSensor);
-    sprintf(buff, "The Gas ratio is %d", analogSensor);  // and finally the integer
-    Serial.write(buff);
-    Serial.write("\n");
-    delay(1000);
-  }
+    if(voice == "send smoke ratio") {
+
+      int analogSensor = analogRead(smokeSensor);
+      sprintf(buff, "The Gas ratio is %d", analogSensor);  // and finally the integer
+      Serial.write(buff);
+      Serial.write("\n");
+      delay(1000);
+    }
   //--------------------------------------------// 
   
-  //----------Turn On One-By-One----------// 
-  else if(voice == "*Lights on") {digitalWrite(lightLED, HIGH);}
-  else if(voice == "*Lights off") {digitalWrite(lightLED, LOW);}
+  //----------Turn On Bedroom OR Bathroom Lights----------// 
+    else if(voice == "bedroom on") {digitalWrite(bedroomLED, HIGH);}
+    else if(voice == "bedroom off") {digitalWrite(bedroomLED, LOW);}
+    else if(voice == "bathroom on") {digitalWrite(bathroomLED, HIGH);}
+    else if(voice == "bathroom off") {digitalWrite(bathroomLED, LOW);}
   //--------------------------------------//
   
+    else if(voice == "all lights on") 
+    {
+      digitalWrite(bedroomLED, HIGH);
+      digitalWrite(bathroomLED, HIGH);
+    }
+    else if(voice == "all lights off") 
+    {
+      digitalWrite(bedroomLED, LOW);
+      digitalWrite(bathroomLED, LOW);
+    }
+   
   //----------Get the temperature data to mobile----------// 
-  else if(voice == "*Send temperature ratio") {
-    int analogSensor = digitalRead(tempSensor);
-    sprintf(buff, "The Temperature ratio is %d", analogSensor);  // and finally the integer
-    Serial.write(buff);
-    Serial.write("\n");
-    delay(1000);  
-  }
+    else if(voice == "send temperature ratio") {
+      int analogSensor = digitalRead(tempSensor);
+      sprintf(buff, "The Temperature ratio is %d", analogSensor);  // and finally the integer
+      Serial.write(buff);
+      Serial.write("\n");
+      delay(1000);  
+    }
   //------------------------------------------------------// 
   
-  voice="";
+    voice="";
     
   }
   
